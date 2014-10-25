546008b8e5d03127092f41c916833dc6f247b2be
smczk <shimon.ni.mail@gmail.com>
2014-10-25 21:28:33 +0900

    Add create method to records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,4 +4,9 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def create
+    Record.create!({ user_id: params[:user_id], point_id: params[:point_id], comment: params[:comment] })
+    render nothing: true
+  end
+
 end
