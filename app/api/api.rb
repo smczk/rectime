f1409895187195c7433083530e175da7579b124c
smczk <shimon.ni.mail@gmail.com>
2014-11-09 13:15:56 +0900

    Add record api

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,7 +3,9 @@ class API < Grape::API
   prefix 'api'
   version 'v1', :using => :path
   format :json
+  content_type :json, "application/json"
 
   mount Movements_API
   mount Points_API
+  mount Records_API
 end
---
+++ app/api/records_api.rb
@@ -0,0 +1,17 @@
+class Records_API < Grape::API
+  resource 'records' do
+    desc "return all records"
+    get do
+      Record.all
+    end
+
+    params do
+      requires :point_id, type: Integer
+      requires :movement_id, type: Integer
+    end
+    desc "register record"
+    post do
+      Record.create!({ point_id: params[:point_id], movement_id: params[:movement_id], comment: params[:comment] })
+    end
+  end
+end
